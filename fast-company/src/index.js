cd4eae1162d8adb3ef34032bba405ff3d9706401
Ivan Miller <therealigmiller@gmail.com>
2022-10-01 23:41:48 +0300

    feat: add app fast-company

--- fast-company/src/index.js
+++ fast-company/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Users from './components/users';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
